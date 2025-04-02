714a25de10d46f43a92f9df8c178c5a1422190e3
Emmett6401 <46923369+Emmett6401@users.noreply.github.com>
2025-04-02 14:32:48 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,51 @@
+from keras.models import load_model  # TensorFlow is required for Keras to work
+import cv2  # Install opencv-python
+import numpy as np
+
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
+
+# Load the model
+model = load_model("model/keras_Model.h5", compile=False)
+
+# Load the labels
+class_names = open("model/labels.txt", "r").readlines()
+
+# CAMERA can be 0 or 1 based on default camera of your computer
+camera = cv2.VideoCapture(0)
+
+while True:
+    # Grab the webcamera's image.
+    ret, image = camera.read()
+
+    # Resize the raw image into (224-height,224-width) pixels
+    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+
+    # Make the image a numpy array and reshape it to the models input shape.
+    image_array = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+
+    # Normalize the image array
+    image_array = (image_array / 127.5) - 1
+
+    # Predicts the model
+    prediction = model.predict(image_array)
+    index = np.argmax(prediction)
+    class_name = class_names[index].strip()
+    confidence_score = prediction[0][index]
+
+    # Overlay class and confidence score on the image
+    text = f"class:{class_name}({int(np.round(confidence_score * 100))}%)"
+    cv2.putText(image, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+
+    # Show the image in a window
+    cv2.imshow("Webcam Image", image)
+
+    # Listen to the keyboard for presses.
+    keyboard_input = cv2.waitKey(1)
+
+    # 27 is the ASCII for the esc key on your keyboard.
+    if keyboard_input == 27:
+        break
+
+camera.release()
+cv2.destroyAllWindows()
